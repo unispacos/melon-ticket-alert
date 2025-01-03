f21beab13c026c87159b0933eba0d748f7a66d2c
ldy9037 <ldy9037@naver.com>
2025-01-03 12:43:30 +0900

    feat: Block 내 잔여좌석 개수 체크 기능 테스트완료

--- check_map_seat.py
+++ check_map_seat.py
@@ -8,15 +8,15 @@ def get_map_seats() -> None:
         'prodId': '210629',
         'pocCode': 'SC0002',
         'scheduleNo': '100001',
-        'blockId': '1807,1702', #getAreaMap.json > seatData > st > sbid
+        'blockId': '576', #getAreaMap.json > seatData > st > sbid
         'corpCodeNo': ''
     }
 
     header = {
         'Accept': 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01',
-        'Content-Length': '71',
+        'Content-Length': '75',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Cookie': '_fwb=157FfrvzGd8P9TaeVuTlmji.1732505114404; PCID=17325051145380684711937; PC_PCID=17325051145380684711937; _T_ANO=LdxO3meWcccuTMykzynf5Ou3ih7+bBk+LXsOne5hgFM1pfyGdzvZ7ycQLYM3424quILz2LMOUnAqIv3x/PXtIuLxXK8Is7h6jK693iG73AhtvjQXL2t9fiVDN6ZoA0A2A/tZco9e8ugZ4pdRMdtAu9Vz00SxNbNFTWpZ5+p96NbuuyqBa5bWkL6ENUToJPsFqOlqeZ8KhjTTG7Zy3l3pZJFnYmRnOFEukF4TZowAgUou4/SQU6a4LNxPfM9/L3yvuWSUMsQVRTMRkAjjchL7fVeM3GGRXX4Z4/PRXerslQxb7ZJTTQeWzSbnUjXlbOPFRV32ZqXmVkNvs7ShjNw/sQ==; MAC=QsRJhbHtBNWlff+Q4A/g20N0tIlJm7Jyprm3ylwPDBwRs+sLDWSMFJO20AOO1Kug; MLCP=MTIzODM4MTAlM0Jyb3poZWtla2VrJTNCJTNCMCUzQmV5SmhiR2NpT2lKSVV6STFOaUo5LmV5SnBjM01pT2lKdFpXMWlaWEl1YldWc2IyNHVZMjl0SWl3aWMzVmlJam9pYldWc2IyNHRhbmQwSWl3aWFXRjBJam94TnpNMU1UazVNVFEyTENKdFpXMWlaWEpMWlhraU9pSXhNak00TXpneE1DSXNJbUYxZEc5TWIyZHBibGxPSWpvaVRpSjkuaU9zR2s4NmwzR29SbDZoUTBZVHZWN0oyYWJsamg0X01URENqWWNjYXBjRSUzQiUzQjIwMjQxMjI2MTY0NTQ2JTNCRW1hRGFtJTNCMSUzQmxkeTkwMzclNDBuYXZlci5jb20lM0IyJTNC; MUS=727344124; keyCookie=12383810; NetFunnel_ID=WP15; store_melon_cupn_check=12383810; TKT_POC_ID=MP15; hide_banner=true; wcs_bt=s_585b06516861:1735528568; JSESSIONID=8302B0282F9A3CB315CC51A94A9516E9',
+        'Cookie': '_fwb=157FfrvzGd8P9TaeVuTlmji.1732505114404; PCID=17325051145380684711937; PC_PCID=17325051145380684711937; _T_ANO=LdxO3meWcccuTMykzynf5Ou3ih7+bBk+LXsOne5hgFM1pfyGdzvZ7ycQLYM3424quILz2LMOUnAqIv3x/PXtIuLxXK8Is7h6jK693iG73AhtvjQXL2t9fiVDN6ZoA0A2A/tZco9e8ugZ4pdRMdtAu9Vz00SxNbNFTWpZ5+p96NbuuyqBa5bWkL6ENUToJPsFqOlqeZ8KhjTTG7Zy3l3pZJFnYmRnOFEukF4TZowAgUou4/SQU6a4LNxPfM9/L3yvuWSUMsQVRTMRkAjjchL7fVeM3GGRXX4Z4/PRXerslQxb7ZJTTQeWzSbnUjXlbOPFRV32ZqXmVkNvs7ShjNw/sQ==; TKT_POC_ID=MP15; cbo=0; MAC=QsRJhbHtBNWlff+Q4A/g20lB7Vmq4to4r3SWlKRjSweZkCdz+xRTweSm4jslZuKy; MLCP=MTIzODM4MTAlM0Jyb3poZWtla2VrJTNCJTNCMCUzQmV5SmhiR2NpT2lKSVV6STFOaUo5LmV5SnBjM01pT2lKdFpXMWlaWEl1YldWc2IyNHVZMjl0SWl3aWMzVmlJam9pYldWc2IyNHRhbmQwSWl3aWFXRjBJam94TnpNMU9EYzFNemc0TENKdFpXMWlaWEpMWlhraU9pSXhNak00TXpneE1DSXNJbUYxZEc5TWIyZHBibGxPSWpvaVRpSjkuQURGMmEtS2QtQ09mUC1jUmhLWG42U1F4QVp5QlJaV1dJT1lLbjBJMndhNCUzQiUzQjIwMjUwMTAzMTIzNjI4JTNCRW1hRGFtJTNCMSUzQmxkeTkwMzclNDBuYXZlci5jb20lM0IyJTNC; MUS=727328307; keyCookie=12383810; hide_banner=true; NetFunnel_ID=WP15; store_melon_cupn_check=12383810; wcs_bt=s_585b06516861:1735875409; JSESSIONID=5ED05E970FDBF842EA7D969DE3FA57D8',
         'Host': 'ticket.melon.com',
         'Referer': 'https://ticket.melon.com/reservation/popup/stepBlock.htm',
         'User-Agent': 'X'
@@ -25,11 +25,12 @@ def get_map_seats() -> None:
     response = requests.post(url,headers=header,data=body)
     map_datas = json.loads(response.text.replace("/**/getSeatListCallBack(","").replace(");", ""))
     count = 0
+
     for st in map_datas['seatData']['st'][0]['ss']:
-        if st['sid'] != 'null': 
+        if st['sid'] != None: 
             count += 1 
+            
     print(count)
-    # 각 좌석 데이터 > seatMapList.json > seatData > st > list (rn : 행 번호)
     return response
 
 get_map_seats()
