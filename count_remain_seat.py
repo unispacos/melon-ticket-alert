5b2ddea90bfb0007a4aba12c15a0e6ab781af6b9
ldy9037 <ldy9037@naver.com>
2025-04-24 20:54:48 +0900

    refactor: 입력값을 글로벌 변수로 분리

--- count_remain_seat.py
+++ count_remain_seat.py
@@ -1,27 +1,35 @@
 import requests
 
+#######################################################
+########### 아래 값 채워준 뒤 실행해주시면 됩니다. #############
+#######################################################
+prodId = ""
+pocCode = ""
+scheduleNo = ""
+cookie = ""
+slack_webhook_url = ""
+#######################################################
+#######################################################
+
 def main() -> None:
     seats = get_seats_summary()
     messages = check_remaining_seats(seats['summary'])
     send_message(messages)
 
 def get_seats_summary() -> None:
-    url = "https://ticket.melon.com/tktapi/product/block/summary.json?v=1" 
+    url = "https://ticket.melon.com/tktapi/product/summary.json?v=1" 
    
     body = {
-        'prodId': '210629',
-        'pocCode': 'SC0002',
-        'scheduleNo': '100001',
-        'perfDate': '',
-        'seatGradeNo': '',
-        'corpCodeNo': ''
+        'prodId': prodId,
+        'pocCode': pocCode,
+        'scheduleNo': scheduleNo
     }
 
     header = {
         'Accept': 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01',
         'Content-Length': '76',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Cookie': '',
+        'Cookie': cookie,
         'Host': 'ticket.melon.com',
         'Referer': 'https://ticket.melon.com/reservation/popup/stepBlock.htm',
         'User-Agent': 'X'
@@ -33,6 +41,8 @@ def get_seats_summary() -> None:
 def check_remaining_seats(seats: list) -> list:
     result = []
     
+    print(seats)
+    
     for seat in seats:
         if seat['realSeatCntlk'] > 0:
             result.append(generate_message(seat))
@@ -40,17 +50,16 @@ def check_remaining_seats(seats: list) -> list:
     return result
 
 def send_message(messages: list) -> None:
-    slack_webhook_url = ""
     for message in messages:
         response = requests.post(slack_webhook_url, json={'text' : message})
    
 def generate_message(seat: dict) -> str: 
     message = ""
-    message += seat['seatGradeName'] + ", " if seat['seatGradeName'] else ""
-    message += seat['floorNo'] if seat['floorNo'] else ""
-    message += seat['floorName'] +  " " if seat['floorName'] else ""
-    message += seat['areaNo'] if seat['areaNo'] else ""
-    message += seat['areaName'] if seat['areaName'] else ""
+    message += seat['seatGradeName'] + ", " if 'seatGradeName' in seat else ""
+    message += seat['floorNo'] if 'floorNo' in seat else ""
+    message += seat['floorName'] +  " " if 'floorName' in seat else ""
+    message += seat['areaNo'] if 'areaNo' in seat else ""
+    message += seat['areaName'] if 'areaName' in seat else ""
     message += "에 잔여좌석 " + str(seat['realSeatCntlk']) + "개 발생! "
     return message
 
